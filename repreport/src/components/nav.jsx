7199d3a75e4df5cf10d78fa2e7c34e3f1066a5b7
Jeffrey Crooks <jeffcrooks@college.harvard.edu>
2025-03-01 14:47:33 -0500

    redesign nav

--- repreport/src/components/nav.jsx
+++ repreport/src/components/nav.jsx
@@ -1,36 +1,44 @@
-
 import Link from 'next/link';
 import styles from '../css/Nav.module.css';
 
 export default function Navbar() {
   return (
     <nav className={styles.nav}>
-      <div className={styles.logo}>
-        <img
-          className={styles.img}
-          src="https://mcusercontent.com/4c0f3950dc0f76487d3277a17/images/29a6dce6-8349-3205-7721-84703e973c06.png"
-          alt="Description"
-        />
-        <h1>RepReport</h1>
-      </div>
+      <div className={styles.navContent}>
+        <div className={styles.logoSection}>
+          <Link href="/" className={styles.logoLink}>
+            <div className={styles.logo}>
+              <div className={styles.logoCircle}>
+                <span className={styles.logoLetter}>R</span>
+              </div>
+              <h1 className={styles.logoText}>RepReport</h1>
+            </div>
+          </Link>
+        </div>
 
-      <div className={styles.links}>
-        {/* Use Next.js Link for internal routing */}
-        <Link href="/" className={styles.linkItem}>
-          Home
-        </Link>
-        <Link href="/bills" className={styles.linkItem}>
-          Bills
-        </Link>
-        {/* External links remain as plain anchor tags */}
-        <a
-          href="https://www.house.gov/representatives/find-your-representative"
-          target="_blank"
-          rel="noopener noreferrer"
-          className={styles.linkItem}
-        >
-          Find My Rep
-        </a>
+        <div className={styles.linksSection}>
+          <Link href="/" className={styles.linkItem}>
+            Home
+          </Link>
+          <Link href="/find-my-rep" className={styles.linkItem}>
+            Find My Rep
+          </Link>
+          <Link href="/bills" className={styles.linkItem}>
+            Bills
+          </Link>
+          <Link href="/newsletters" className={styles.linkItem}>
+            Newsletters
+          </Link>
+          <Link href="/about" className={styles.linkItem}>
+            About
+          </Link>
+        </div>
+
+        <div className={styles.signupSection}>
+          <Link href="/signup" className={styles.signupButton}>
+            Sign Up
+          </Link>
+        </div>
       </div>
     </nav>
   );
